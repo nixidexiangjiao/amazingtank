deb45a932a50580c2d8b6188497ecc6986af37be
h198798337 <h198798337@126.com>
2014-07-06 14:23:42 +0800

    初始导入

---
+++ desktop/src/com/hyh/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hyh.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hyh.game.amazingtank;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new amazingtank(), config);
+	}
+}
